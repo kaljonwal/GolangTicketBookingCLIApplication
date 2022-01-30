25a6f87b6fd3dfaeafb0923a83b8696b942800ba
kalyan jonwal <kalyan.jonwal@aquasec.com>
2022-01-30 18:58:04 +0530

    added booking logic

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName string = "Go Conference"
-	const conferenceTicket = 50
-	var remainingTicket = conferenceTicket
+	const conferenceTicket uint = 50
+	var remainingTicket uint = conferenceTicket
+	var bookings []string
 
 	//print the type of variable
 	fmt.Printf("conferenceTicket is type of %T, remainingTicket is type of %T\n", conferenceTicket, remainingTicket)
@@ -14,9 +18,45 @@ func main() {
 	fmt.Println("We have total of", conferenceTicket, "tickets and", remainingTicket, "are still available")
 	fmt.Println("Book your ticket to attend")
 
-	var userName string
-	var numberOfTickets int
-	userName = "kalyan"
-	numberOfTickets = 2
-	fmt.Printf("%v booked the %v tickets\n", userName, numberOfTickets)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var numberOfTickets uint
+
+		fmt.Println("Enter your first name ")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address")
+		fmt.Scan(&email)
+
+		fmt.Println("How many tickets you want")
+		fmt.Scan(&numberOfTickets)
+
+		if numberOfTickets >= remainingTicket {
+			fmt.Printf("we have only %v tickets, so you can't book %v tickets\n please try again \n", remainingTicket, numberOfTickets)
+			continue
+		}
+		remainingTicket = remainingTicket - numberOfTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you  %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, numberOfTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTicket, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("first names of bookings are : %v\n", firstNames)
+
+		if remainingTicket == 0 {
+			//end program
+			fmt.Println("Our conferebce is booked out, Thank you for overehelming response")
+			break
+		}
+	}
 }
--- readme.md
+++ readme.md
@@ -6,4 +6,32 @@ Notes for go:
 Go is a statically typed language
     you'll need to tell compiler, the data type when declaring variable else you'll get error
     Go can imply the type based on value
-    Values will be validated at compile time only lets say if you are using negative
+    Values will be validated at compile time only, lets say if you are using negative for unsigned int then it will show error
+    if you want to define type explicitly or constant then in that case := will not work
+
+Take input from  user
+    fmt.Scan(userName) : save user input in variable called userName. but if we run the program with this statement program will not wait for user input so you'll need to add &(pointer) before variable name (fmt.scan(userName))
+
+Pointer:
+    A pointer is a variable that points to the memory address of another variable
+
+Arrays and slices: 
+    Array: Fixed in size, only same type of element
+
+range : range allows us to iterate over element for different data structure (not only arrays and slices)
+
+strings.Fields():
+    split the string with white space as separator
+    and return a slice with the split element
+    e.g name := "kalyan jonwal"
+        strings.Fields(name) it will return ["kalyan","jonwal"]
+loop:
+    for loop:
+        infinite loop:
+            for {
+
+            }
+    for-each loop:
+        for index, ele := range Array/slice/DifferentdataStructureName{
+
+        }
